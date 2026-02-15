89407d7538796a4cf38ed1ec4c3df777f6631019
cal21 <c21c1@duck.com>
2026-02-15 01:50:22 +0000

    Added CSS styling

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,13 @@
 import Logo from './Logo.svg';
+import Nav from './Nav';
 
 function Header() {
     return (
         <header>
-            <h1>Little Lemon</h1>
-            <img src={Logo} alt="Little Lemon Logo" />
+            <h1>
+                <img src={Logo} alt="Little Lemon Logo" />
+            </h1>
+            <Nav />
         </header>
     )
 }
